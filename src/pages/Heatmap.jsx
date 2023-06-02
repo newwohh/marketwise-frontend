558dc576060018bd0b9976b113b340d77532e448
newwohh <nevoznvx@gmail.com>
2023-06-02 18:08:28 +0530

    add

--- src/pages/Heatmap.jsx
+++ src/pages/Heatmap.jsx
@@ -22,9 +22,11 @@ const bull = (
 
 function Heatmap(props) {
   console.log(props.cryptomap);
+  let maps = props.cryptomap;
   const HeatmapClass = useHeatmap();
   const a = parseFloat(props.cryptomap[0].percent_change_24h);
   const b = parseFloat(props.cryptomap[0].percent_change_1h);
+  console.log(a, b);
   return (
     <React.Fragment>
       <NavBar />
@@ -53,35 +55,41 @@ function Heatmap(props) {
           <div>
             <div>
               <Box>
-                <Card variant="outlined">
-                  <CardContent
-                    className={
-                      a > b ? HeatmapClass.cardStyle : HeatmapClass.cardStyletwo
-                    }
-                  >
-                    <Typography
-                      sx={{ fontSize: 14 }}
-                      color="text.secondary"
-                      gutterBottom
-                    >
-                      Word of the Day
-                    </Typography>
-                    <Typography variant="h5" component="div">
-                      be{bull}nev{bull}o{bull}lent
-                    </Typography>
-                    <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                      adjective
-                    </Typography>
-                    <Typography variant="body2">
-                      well meaning and kindly.
-                      <br />
-                      {'"a benevolent smile"'}
-                    </Typography>
-                  </CardContent>
-                  <CardActions>
-                    <Button size="small">Learn More</Button>
-                  </CardActions>
-                </Card>
+                {maps.map((el, i) => {
+                  return (
+                    <Card variant="outlined" sx={{ width: "400px" }}>
+                      <CardContent
+                        className={
+                          el.percent_change_1h > 0
+                            ? HeatmapClass.cardStyle
+                            : HeatmapClass.cardStyletwo
+                        }
+                      >
+                        <Typography
+                          sx={{ fontSize: 14 }}
+                          color="text.secondary"
+                          gutterBottom
+                        >
+                          Word of the Day
+                        </Typography>
+                        <Typography variant="h5" component="div">
+                          be{bull}nev{bull}o{bull}lent
+                        </Typography>
+                        <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                          adjective
+                        </Typography>
+                        <Typography variant="body2">
+                          well meaning and kindly.
+                          <br />
+                          {'"a benevolent smile"'}
+                        </Typography>
+                      </CardContent>
+                      <CardActions>
+                        <Button size="small">Learn More</Button>
+                      </CardActions>
+                    </Card>
+                  );
+                })}
               </Box>
             </div>
           </div>

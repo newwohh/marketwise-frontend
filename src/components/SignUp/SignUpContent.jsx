2f345e5a27afd527461c7e7aafe889d6e4ef2c1c
newwohh <nevoznvx@gmail.com>
2023-06-07 22:09:55 +0530

    add

--- src/components/SignUp/SignUpContent.jsx
+++ src/components/SignUp/SignUpContent.jsx
@@ -1,7 +1,74 @@
+import { Button, TextField, Typography } from "@mui/material";
 import React from "react";
+import { Link } from "react-router-dom";
+import useSignUp from "../../styles/SignUp/SignUp";
 
 function SignUpContent() {
-  return <div>SignUpContent</div>;
+  const signUpClass = useSignUp();
+  return (
+    <main style={{ backgroundColor: "#F0F8FF", height: "1000px" }}>
+      <section className={signUpClass.mainsection}>
+        <div style={{ marginTop: "200px" }}>
+          <form className={signUpClass.form}>
+            <Typography variant="h4">Sign-In</Typography>
+            <div style={{ marginTop: "20px" }}>
+              <TextField type="text" sx={{ width: "450px" }} label="username" />
+            </div>
+            <div style={{ marginTop: "20px" }}>
+              <TextField
+                type="password"
+                sx={{ width: "450px" }}
+                label="password"
+              />
+            </div>
+            <div>
+              <Button
+                sx={{
+                  width: "450px",
+                  height: "50px",
+                  margin: "20px",
+                  color: "white",
+                  backgroundColor: "#002244",
+                  "&:hover": {
+                    border: "1px solid #002244",
+                    backgroundColor: "#F0F8FF",
+                    color: "#002244",
+                  },
+                }}
+              >
+                Login
+              </Button>
+            </div>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <Link
+                style={{
+                  textDecoration: "none",
+                  color: "#002244",
+                }}
+              >
+                <Typography>Not Registered ? </Typography>
+              </Link>
+              <Link
+                style={{
+                  marginLeft: "30px",
+                  textDecoration: "none",
+                  color: "#002244",
+                }}
+              >
+                <Typography>Forgot Password ?</Typography>
+              </Link>
+            </div>
+          </form>
+        </div>
+      </section>
+    </main>
+  );
 }
 
 export default SignUpContent;
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import SignInNavbar from "../components/SignIn/SignInNavBar/SignInNavbar";
+import SignUpContent from "../components/SignUp/SignUpContent";
 
 function SignUp() {
   return (
     <React.Fragment>
       <SignInNavbar />
+      <SignUpContent />
     </React.Fragment>
   );
 }

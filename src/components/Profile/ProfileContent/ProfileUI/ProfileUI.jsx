5d171d83613d698b7381012663bca5bcc1660835
newwohh <nevoznvx@gmail.com>
2023-06-10 13:33:20 +0530

    add

--- src/components/Profile/ProfileContent/ProfileUI/ProfileUI.jsx
+++ src/components/Profile/ProfileContent/ProfileUI/ProfileUI.jsx
@@ -1,6 +1,14 @@
 import React from "react";
 import user from "../../../../assets/MyApril6.jpg";
-import { Button, Divider, Typography } from "@mui/material";
+import {
+  Button,
+  Divider,
+  List,
+  ListItem,
+  ListItemText,
+  Typography,
+  TextField,
+} from "@mui/material";
 
 function ProfileUI() {
   return (
@@ -10,6 +18,7 @@ function ProfileUI() {
           display: "flex",
           justifyContent: "center",
           textAlign: "center",
+          marginBottom: "30px",
         }}
       >
         <img
@@ -24,12 +33,81 @@ function ProfileUI() {
           display: "flex",
           justifyContent: "center",
           textAlign: "center",
+          marginTop: "20px",
+          marginBottom: "30px",
         }}
       >
-        <Button>Edit Profile</Button>
-        <Button>Preview</Button>
+        <Button
+          sx={{
+            width: "200px",
+            height: "50px",
+            borderRadius: "30px",
+            color: "#002244",
+          }}
+        >
+          Edit Profile
+        </Button>
+        <Button
+          sx={{
+            width: "200px",
+            height: "50px",
+            borderRadius: "30px",
+            color: "#002244",
+          }}
+        >
+          Preview
+        </Button>
       </div>
       <Divider />
+      <div
+        style={{
+          textAlign: "center",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <List sx={{ width: "800px" }}>
+          <ListItem>
+            <ListItemText>Name</ListItemText>
+            <ListItemText>
+              <TextField
+                id="outlined-read-only-input"
+                defaultValue={"Name"}
+                InputProps={{
+                  readOnly: true,
+                }}
+              />
+            </ListItemText>
+          </ListItem>
+          <ListItem>
+            <ListItemText>E-Mail</ListItemText>
+            <ListItemText>
+              <TextField
+                id="outlined-read-only-input"
+                defaultValue="E-Mail"
+                InputProps={{
+                  readOnly: true,
+                }}
+              />
+            </ListItemText>
+          </ListItem>
+          <ListItem>
+            <ListItemText sx={{ width: "100px" }}>Password</ListItemText>
+            <ListItemText>
+              <TextField
+                id="outlined-read-only-input"
+                defaultValue="Password"
+                InputProps={{
+                  readOnly: true,
+                }}
+                sx={{ width: "300px" }}
+              />
+            </ListItemText>
+          </ListItem>
+        </List>
+      </div>
     </div>
   );
 }
